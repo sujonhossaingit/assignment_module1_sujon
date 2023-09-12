ce28f0d14df9e272e0b9d623ba7ecda8fc1b286c
Md Sujon Hossain <119471863+sujonhossaingit@users.noreply.github.com>
2023-09-12 14:40:16 +0600

    Update personal_info.php

--- personal_info.php
+++ personal_info.php
@@ -1,18 +1,17 @@
-    <?php
-    // Define variables for information
-    $name = "Md Sujon Hossain";
-    $age = 33; 
-    $country = "Bangladesh"; 
-    $introduction = "Hello! I am $name. I am $age years old and I come from $country. Working as an Assistant Programmer.";
+<?php
+// Define variables for information
+$name = "Md Sujon Hossain";
+$age = 33; 
+$country = "Bangladesh"; 
+$introduction = "Hello! I am $name. I am $age years old and I come from $country. Working as an Assistant Programmer.";
 
-    // Displaing the personal information using echo
-    echo "Personal Information";
-    echo PHP_EOL;
-    echo "Name: $name";
-    echo PHP_EOL;
-    echo "Age: $age";
-    echo PHP_EOL;
-    echo "Country: $country";
-    echo PHP_EOL;
-    echo "Introduction: $introduction";
-    ?>
+// Displaing the personal information using echo
+echo "Personal Information: ";
+echo "\n";
+echo "Name: $name";
+echo "\n";
+echo "Age: $age";
+echo "\n";
+echo "Country: $country";
+echo "\n";
+echo "Introduction: $introduction";
