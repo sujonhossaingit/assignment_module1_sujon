55fee09fc465128fd5f4f0e5d04ab6234e61468e
Md Sujon Hossain <119471863+sujonhossaingit@users.noreply.github.com>
2023-09-12 11:08:47 +0600

    Add files via upload

---
+++ personal_info.php
@@ -0,0 +1,18 @@
+    <?php
+    // Define variables for information
+    $name = "Md Sujon Hossain";
+    $age = 33; 
+    $country = "Bangladesh"; 
+    $introduction = "Hello! I am $name. I am $age years old and I come from $country. Working as an Assistant Programmer.";
+
+    // Displaing the personal information using echo
+    echo "Personal Information";
+    echo PHP_EOL;
+    echo "Name: $name";
+    echo PHP_EOL;
+    echo "Age: $age";
+    echo PHP_EOL;
+    echo "Country: $country";
+    echo PHP_EOL;
+    echo "Introduction: $introduction";
+    ?>
